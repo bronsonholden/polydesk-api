3b24b45d40e625f4d584934ace4961ebab189e02
Paul Holden <paul@holdensoftware.com>
2018-12-19 23:46:46 -0800

    Add Document model and uploader

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :test do
 end
 
 gem 'carrierwave', '~> 0.11.2'
+gem 'fog-aws'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+CarrierWave.configure do |config|
+  if Rails.env.test?
+    config.storage = :file
+  end
+
+  if Rails.env.development? || Rails.env.production?
+    config.storage = :fog
+    config.fog_directory = Rails.application.credentials[Rails.env.to_sym][:aws][:bucket]
+
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials[Rails.env.to_sym][:aws][:access_key_id],
+      aws_secret_access_key: Rails.application.credentials[Rails.env.to_sym][:aws][:secret_access_key],
+      region: Rails.application.credentials[Rails.env.to_sym][:aws][:region]
+    }
+  end
+end
