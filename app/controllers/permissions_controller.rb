ecd49c7fcc979480245d67697378868ba7158822
Paul Holden <paul@holdensoftware.com>
2019-01-06 13:35:42 -0800

    Rescue from RecordNotFound exceptions

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -1,6 +1,9 @@
 class PermissionsController < ApplicationController
   # User must be authenticated before they can interact with permissions
   before_action :authenticate_user!
+  before_action :set_account
+  before_action :set_user
+  before_action :set_account_user
 
   def new
   end
@@ -28,11 +31,34 @@ class PermissionsController < ApplicationController
   end
 
   private
+    def set_account
+      @account = Account.find_by! identifier: params[:identifier]
+    rescue ActiveRecord::RecordNotFound
+      render json: {errors: {account: ['does not exist']}},
+             status: :unprocessable_entity
+      return
+    end
+
+    def set_user
+      @user = User.find_by! id: params[:id]
+    rescue ActiveRecord::RecordNotFound
+      render json: {errors: {user: ['does not exist']}},
+             status: :unprocessable_entity
+      return
+    end
+
+    def set_account_user
+      @account_user = AccountUser.find_by! account_id: @account.id,
+                                           user_id: @user.id
+    rescue ActiveRecord::RecordNotFound
+      render json: {errors: {user: ['does not have access to this account']}},
+             status: :unprocessable_entity
+      return
+    end
+
     def permission_params
-      account = Account.find_by identifier: params[:identifier]
-      account_user = AccountUser.find_by account_id: account.id, user_id: params[:id]
       p = params.require(:permission).permit(:code)
-      p[:account_user_id] = account_user.id
+      p[:account_user_id] = @account_user.id
       return p
     end
 end
