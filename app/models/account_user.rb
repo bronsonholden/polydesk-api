e64067a4372efa956ed835407a08de0f3ccd9530
Paul Holden <paul@holdensoftware.com>
2019-01-06 23:56:36 -0800

    Add model validations for uniqueness
    - Validate uniqueness of AccountUser account in user scope
    - Validate uniqueness of Permission code in account_user_id scope

--- app/models/account_user.rb
+++ app/models/account_user.rb
@@ -1,6 +1,6 @@
 class AccountUser < ApplicationRecord
   validates :user_id, presence: true
-  validates :account_id, presence: true
+  validates :account_id, presence: true, uniqueness: { scope: :user_id }
   belongs_to :user
   belongs_to :account
   has_many :permissions
--- app/models/permission.rb
+++ app/models/permission.rb
@@ -5,7 +5,7 @@ class Permission < ApplicationRecord
     :document_index
   ]
 
-  validates :code, presence: true
+  validates :code, presence: true, uniqueness: { scope: :account_user_id }
   validates :account_user_id, presence: true
   # Set foreign key to excluded AccountUser model
   belongs_to :account_user, class_name: 'AccountUser', foreign_key: 'account_user_id'
