e9ab47df5e3901d4984fd6f08dd01eb3743bacef
Paul Holden <paul@holdensoftware.com>
2019-01-06 23:24:31 -0800

    Add null constraint to Folders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_07_071432) do
+ActiveRecord::Schema.define(version: 2019_01_07_072400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2019_01_07_071432) do
   end
 
   create_table "folders", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.bigint "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
