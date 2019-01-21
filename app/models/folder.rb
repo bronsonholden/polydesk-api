2b747d17999c5f94ab4e2bdd0ee22ef40660ae0a
Paul Holden <paul@holdensoftware.com>
2019-01-20 21:55:30 -0800

    Enforce unique folder names throughout structure

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,5 +1,7 @@
 class Folder < ApplicationRecord
+  alias_attribute :parent_folder, :parent_id
   belongs_to :parent, class_name: 'Folder', optional: true
+  before_save :default_parent
   has_many :children, class_name: 'Folder', foreign_key: 'parent_id'
   has_many :documents, through: :folder_documents
   validates :name, presence: true, format: {
@@ -8,4 +10,12 @@ class Folder < ApplicationRecord
     with: /\A([A-Za-z0-9\-\(\)\[\]'_][A-Za-z0-9 \-\(\)\[\]'_\.]*[A-Za-z0-9\-\(\)\[\]'_]|[A-Za-z0-9\-\(\)\[\]\|'_]{1,2})\z/,
     message: 'may only contain alphanumerals, spaces, or the following: _ . - ( ) [ ] and may not start or end with a space or .'
   }
+
+  validates_each :parent_id do |record, attr, value|
+    record.errors.add('parent_folder', 'does not exist') unless value.zero? or Folder.find_by_id(value)
+  end
+
+  def default_parent
+    self.parent_id ||= 0
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_07_074705) do
+ActiveRecord::Schema.define(version: 2019_01_20_213236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,9 +50,10 @@ ActiveRecord::Schema.define(version: 2019_01_07_074705) do
 
   create_table "folders", force: :cascade do |t|
     t.string "name", null: false
-    t.bigint "parent_id"
+    t.bigint "parent_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["parent_id", "name"], name: "index_folders_on_parent_id_and_name", unique: true
     t.index ["parent_id"], name: "index_folders_on_parent_id"
   end
 
