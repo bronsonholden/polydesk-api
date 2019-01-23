878289bb55fe8f7db925055c2d649c0726fd162d
Paul Holden <paul@holdensoftware.com>
2019-01-22 22:09:53 -0800

    Working on tests

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class AccountsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Test account and user creation' do
+    post '/accounts', params: { account_name: 'Controller Test',
+                                account_identifier: 'controller',
+                                user_name: 'Controller User',
+                                user_email: 'controller@polydesk.io',
+                                password: 'password',
+                                password_confirmation: 'password' }
+    assert_response :success
+  end
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Setup account exist' do
+    assert @account
+  end
+
+  test 'Setup account configured correctly' do
+    assert @account.name = 'Test Account'
+    assert @account.identifier = 'test'
+  end
 end
