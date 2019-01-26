ad520a634426b2fd53cc8fad217a5c30fee1b422
Paul Holden <paul@holdensoftware.com>
2019-01-25 23:26:14 -0800

    Add document upload test

--- spec/requests/documents_spec.rb
+++ spec/requests/documents_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe 'Documents', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'POST /rspec/documents' do
+    it 'uploads a new document' do
+      file = Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/compressed.tracemonkey-pldi-09.pdf'))
+      post '/rspec/documents', headers: account_login('rspec', 'rspec@polydesk.io', 'password'),
+                               params: { content: file }
+      expect(response).to have_http_status(200)
+    end
+  end
 end
