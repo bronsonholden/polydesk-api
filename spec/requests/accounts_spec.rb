53b18fc531beb5132acf3d65d451b1248e001251
Paul Holden <paul@holdensoftware.com>
2019-01-25 19:25:20 -0800

    Remove unnecessary before(:each)

--- spec/requests/accounts_spec.rb
+++ spec/requests/accounts_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Accounts', type: :request do
-  before(:each) do
-    @account = Account.find_by_identifier 'rspec'
-  end
-
   describe 'GET /rspec/account' do
     it 'retrieves account information' do
       get '/rspec/account', headers: account_login('rspec', 'rspec@polydesk.io', 'password')
