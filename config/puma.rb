b8b83a571af98a9dfacbbe1b01b5a8dc8dfecaa3
Paul Holden <paul@holdensoftware.com>
2019-01-27 22:05:31 -0800

    Add daemonization to puma config

--- config/puma.rb
+++ config/puma.rb
@@ -32,3 +32,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+daemonize true
+pidfile '/tmp/polydesk-api.pid'
