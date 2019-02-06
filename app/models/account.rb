72c86a5c072d026503b3cda55bb96100fe2aa4a9
Paul Holden <paul@holdensoftware.com>
2019-02-05 18:48:39 -0800

    Validation on account identifiers
    - Also add some test cases

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,13 @@ class Account < ApplicationRecord
   alias_attribute :account_identifier, :identifier
   alias_attribute :account_name, :name
   attr_readonly :identifier
-  validates :identifier, uniqueness: true, presence: true, length: { minimum: 3, maximum: 20}
+  validates :identifier, uniqueness: true,
+                         presence: true,
+                         length: { minimum: 3, maximum: 20 },
+                         format: {
+                           with: /\A[a-z][a-z0-9]+\z/,
+                           message: 'may only contain lowercase alphanumerals and must begin with a letter'
+                         }
   validates :name, presence: true
   has_many :account_users
   has_many :users, through: :account_users, dependent: :destroy
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -9,4 +9,21 @@ class AccountTest < ActiveSupport::TestCase
     assert @account.name = 'Test Account'
     assert @account.identifier = 'test'
   end
+
+  test 'Disallow invalid account identifiers' do
+    assert_not Account.create(name: 'Invalid 1', identifier: '2name').valid?
+    assert_not Account.create(name: 'Invalid 2', identifier: 'a name').valid?
+    assert_not Account.create(name: 'Invalid 3', identifier: 'a-name').valid?
+    assert_not Account.create(name: 'Invalid 4', identifier: 'a/name').valid?
+    assert_not Account.create(name: 'Invalid 5', identifier: 'a+name').valid?
+    assert_not Account.create(name: 'Invalid 6', identifier: 'a!name').valid?
+    assert_not Account.create(name: 'Invalid 7', identifier: 'a.name').valid?
+    assert_not Account.create(name: 'Invalid 8', identifier: 'id').valid?
+  end
+
+  test 'Allow valid account identifiers' do
+    assert Account.create(name: 'Valid 1', identifier: 'valid1').valid?
+    assert Account.create(name: 'Valid 2', identifier: 'valid').valid?
+    assert Account.create(name: 'Valid 3', identifier: 'val').valid?
+  end
 end
