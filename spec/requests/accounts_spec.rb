c097e4aaa1b45bba18b1ed3b232a6a996e7165eb
Paul Holden <paul@holdensoftware.com>
2019-02-05 23:01:05 -0800

    RSpec test: account creation

--- spec/requests/accounts_spec.rb
+++ spec/requests/accounts_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Accounts', type: :request do
+  describe 'POST /accounts' do
+    it 'creates new account' do
+      post '/accounts', params: { account_identifier: 'rspec2',
+                                  account_name: 'RSpec 2',
+                                  user_name: 'RSpec User 2',
+                                  user_email: 'rspec2@polydesk.io',
+                                  password: 'password',
+                                  password_confirmation: 'password' }
+      expect(response).to have_http_status(201)
+    end
+  end
+
   describe 'GET /rspec/account' do
     it 'retrieves account information' do
       get '/rspec/account', headers: account_login('rspec', 'rspec@polydesk.io', 'password')
