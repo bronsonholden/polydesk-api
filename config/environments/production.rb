5dfe3ebf4bb9e2dbbadf0671e582dac978eb93b3
Paul Holden <paul@holdensoftware.com>
2019-02-13 00:50:44 -0800

    Work on getting production working
    - Using default user/pass for local DB
    - Add AWS credentials for S3 access

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.routes.default_url_options[:host] = 'api.polydesk.io'
+  Rails.application.routes.default_url_options[:protocol] = 'http'
 end
