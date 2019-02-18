09aabb34aca36dbe46415199847ac2fb0e2ad88f
Paul Holden <paul@holdensoftware.com>
2019-02-17 17:45:23 -0800

    Serialize default account for User

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,11 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :email
+
+  # Only want to show default account identifier
+  attribute :default_account do |user|
+    user.default_account.identifier
+  end
+
   has_many :accounts
 end
