351fed383a8805963cd7b573d55ad54d8b8d75c1
Paul Holden <paul@holdensoftware.com>
2019-03-27 21:24:53 -0700

    FormSerializer returns self link

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,11 @@
 class Form < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   validates :name, presence: true, uniqueness: true
   validates :schema, presence: true
   validates :layout, presence: true
+
+  def url
+    form_url(id: self.id, identifier: Apartment::Tenant.current)
+  end
 end
--- app/serializers/form_serializer.rb
+++ app/serializers/form_serializer.rb
@@ -1,4 +1,7 @@
 class FormSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :schema, :layout, :created_at, :updated_at
+  link :self, -> (form) {
+    form.url
+  }
 end
