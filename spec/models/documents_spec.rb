b71c22ab638e28cbd870626e9354056e0bcc7d73
Paul Holden <paul@holdensoftware.com>
2019-04-17 21:39:15 -0700

    Add some model spec tests

--- spec/models/documents_spec.rb
+++ spec/models/documents_spec.rb
@@ -7,4 +7,32 @@ describe Document do
       expect { document.destroy }.to change(Folder, :count).by(0)
     end
   end
+
+  describe 'validations' do
+    context 'with parent folder' do
+      let!(:document) { create :subdocument, name: 'Subdocument' }
+
+      it 'prevents duplicate name' do
+        file = Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/fox.txt'))
+        expect { document.folder.documents.create!(content: file, name: 'Subdocument') }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+
+      it 'requires content' do
+        expect { Document.create!(name: 'No Content') }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context 'without parent folder' do
+      let!(:original) { create :document, name: 'Document' }
+
+      it 'prevents duplicate name' do
+        file = Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/fox.txt'))
+        expect { Document.create!(content: file, name: 'Document') }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+
+      it 'requires content' do
+        expect { Document.create!(name: 'No Content') }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
 end
--- spec/models/folders_spec.rb
+++ spec/models/folders_spec.rb
@@ -21,4 +21,20 @@ describe Folder do
       expect { document.folder.destroy }.to change(Document, :count).by(-1)
     end
   end
+
+  describe 'validations' do
+    context 'with parent folder' do
+      let!(:folder) { create :subfolder, name: 'Subfolder' }
+      it 'prevents duplicate name' do
+        expect { folder.folders.create!(name: 'Subfolder') }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context 'without parent folder' do
+      let!(:folder) { create :folder, name: 'Folder' }
+      it 'prevents duplicate name' do
+        expect { Folder.create!(name: 'Document') }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
 end
