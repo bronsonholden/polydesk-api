ea0840bee36c69a5c2779e2b90a9d16b90466d46
Paul Holden <paul@holdensoftware.com>
2019-05-05 23:00:40 -0700

    Comment fix

--- app/uploader/document_content_uploader.rb
+++ app/uploader/document_content_uploader.rb
@@ -7,7 +7,7 @@ class DocumentContentUploader < Shrine
   end
 
   Attacher.promote { |data|
-    # Store tenant so it can activate it before uploading to storage
+    # Store tenant so it can be activated before file is promoted to storage
     data[:tenant] = Apartment::Tenant.current
     dispatcher = Polydesk::JobDispatcher.get
     dispatcher.new.dispatch(DocumentContentBackgroundUploader, data)
