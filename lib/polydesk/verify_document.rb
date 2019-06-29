4e91585cf62b814a23532b178450648f8135534e
Paul Holden <paul@holdensoftware.com>
2019-06-29 12:29:06 -0700

    Re-enable storage limit validation, but hardcoded 1 GB
    Will re-implement Options soon

--- lib/polydesk/verify_document.rb
+++ lib/polydesk/verify_document.rb
@@ -1,13 +1,11 @@
 module Polydesk
   module VerifyDocument
-    # TODO: Improve this. Difficult to disable on model outside of just
-    # commenting the code
     def within_storage_limit
-      # option = Option.find_by_name :document_storage_limit
-      # limit = (option.value.to_i unless option.nil?) || 1e9 # Hardcoded limit
-      # if Document.sum(:file_size) + self.file_size > limit
-      #   raise Polydesk::ApiExceptions::DocumentException::StorageLimitReached.new(self)
-      # end
+      # Hardcoded limit for now
+      limit = 1e9
+      if Document.sum(:file_size) + self.file_size > limit
+        raise Polydesk::ApiExceptions::DocumentException::StorageLimitReached.new(self)
+      end
     end
   end
 end
