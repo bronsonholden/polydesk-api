67cf26913bc103533404eae11d53781e7d7f07b7
Paul Holden <paul@holdensoftware.com>
2019-07-04 09:17:36 -0700

    Fix no-content document uploads

--- lib/polydesk/verify_document.rb
+++ lib/polydesk/verify_document.rb
@@ -3,6 +3,9 @@ module Polydesk
     def within_storage_limit
       # Hardcoded limit for now
       limit = 1e9
+      # Allow no-content documents
+      return if self.file_size.nil?
+      # Verify within storage limit
       if Document.sum(:file_size) + self.file_size > limit
         raise Polydesk::ApiExceptions::DocumentException::StorageLimitReached.new(self)
       end
