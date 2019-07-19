01a4b95d6043ee763fa44401640e4fd9007acf01
Paul Holden <paul@holdensoftware.com>
2019-07-18 21:55:14 -0700

    Clean up Account spec

--- spec/requests/accounts_spec.rb
+++ spec/requests/accounts_spec.rb
@@ -1,27 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Accounts', type: :request do
+  let(:attributes) {
+    {
+      identifier: 'rspec2',
+      name: 'RSpec 2'
+    }
+  }
+
+  let(:data) {
+    {
+      type: 'accounts',
+      attributes: attributes
+    }
+  }
+
+  let(:params) {
+    {
+      data: data
+    }
+  }
+
+  let(:account) { Account.first }
+
   describe 'POST /accounts' do
     it 'creates new account with password' do
       post '/accounts', headers: rspec_session,
-                        params: {
-                          data: {
-                            type: 'accounts',
-                            attributes: {
-                              identifier: 'rspec2',
-                              name: 'RSpec 2' } } }.to_json
+                        params: params.to_json
       expect(response).to have_http_status(201)
       expect(AccountUser.last.role).to eq('administrator')
     end
 
     it 'creates new account without password' do
       post '/accounts', headers: rspec_session,
-                        params: {
-                          data: {
-                            type: 'accounts',
-                            attributes: {
-                              identifier: 'rspec2',
-                              name: 'RSpec 2' } } }.to_json
+                        params: params.to_json
       expect(response).to have_http_status(201)
     end
   end
@@ -34,17 +46,25 @@ RSpec.describe 'Accounts', type: :request do
   end
 
   describe 'PATCH /accounts/:id' do
+    let(:data) {
+      {
+        id: account.id.to_s,
+        type: 'accounts',
+        attributes: attributes
+      }
+    }
+
+    let(:attributes) {
+      {
+        name: 'RSpec Renamed'
+      }
+    }
+
     context 'with permission' do
       let!(:permission) { create :permission, code: :account_update, account_user: AccountUser.last }
       it 'updates account information' do
-        account = Account.first
         patch "/accounts/#{account.id}", headers: rspec_session,
-                                         params: {
-                                           data: {
-                                             id: account.id.to_s,
-                                             type: 'accounts',
-                                               attributes: {
-                                                 name: 'RSpec Renamed' } } }.to_json
+                                         params: params.to_json
         expect(response).to have_http_status(200)
         expect(account).to have_changed_attributes
         expect(account.reload.name).to eq('RSpec Renamed')
@@ -54,14 +74,8 @@ RSpec.describe 'Accounts', type: :request do
     context 'guest with permission' do
       let!(:guest) { create :rspec_guest, set_permissions: [:account_update] }
       it 'returns authorization error' do
-        account = Account.first
         patch "/accounts/#{account.id}", headers: rspec_session(guest),
-                                         params: {
-                                           data: {
-                                             id: account.id.to_s,
-                                             type: 'accounts',
-                                               attributes: {
-                                                 name: 'RSpec Renamed' } } }.to_json
+                                         params: params.to_json
         expect(response).to have_http_status(403)
       end
     end
@@ -71,12 +85,7 @@ RSpec.describe 'Accounts', type: :request do
       it 'updates account information' do
         account = Account.first
         patch "/accounts/#{account.id}", headers: rspec_session(admin),
-                                         params: {
-                                           data: {
-                                             id: account.id.to_s,
-                                             type: 'accounts',
-                                               attributes: {
-                                                 name: 'RSpec Renamed' } } }.to_json
+                                         params: params.to_json
         expect(response).to have_http_status(200)
         expect(account).to have_changed_attributes
         expect(account.reload.name).to eq('RSpec Renamed')
@@ -85,14 +94,8 @@ RSpec.describe 'Accounts', type: :request do
 
     context 'without permission' do
       it 'returns authorization error' do
-        account = Account.last
         patch "/accounts/#{account.id}", headers: rspec_session,
-                                         params: {
-                                           data: {
-                                             id: account.id.to_s,
-                                             type: 'accounts',
-                                               attributes: {
-                                                 name: 'RSpec Renamed' } } }.to_json
+                                         params: params.to_json
         expect(response).to have_http_status(403)
       end
     end
