045d7c90e5035c12bb298787f3f2eb787e0906bb
Paul Holden <paul@holdensoftware.com>
2019-07-26 21:16:23 -0700

    Add owner attribute to DocumentSerializer
    Prep for document check-out/in functionality

--- app/serializers/document_serializer.rb
+++ app/serializers/document_serializer.rb
@@ -1,5 +1,10 @@
 class DocumentSerializer < TenantSerializer
   attributes :content_type, :file_size, :created_at, :updated_at, :name, :discarded_at
   attribute :folder_id
+
+  attribute :owner do
+    nil
+  end
+
   has_one :folder
 end
