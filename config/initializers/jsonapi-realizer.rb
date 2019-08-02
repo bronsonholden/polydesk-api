65812182dac81440998fa5629deedba2edd606ef
Paul Holden <paul@holdensoftware.com>
2019-08-01 21:37:19 -0700

    Fix pagination
    jsonapi-realizer uses page instead of offset. Add 1 to offset to get page

---
+++ config/initializers/jsonapi-realizer.rb
@@ -0,0 +1,8 @@
+JSONAPI::Realizer.configuration do |config|
+end
+
+module JSONAPI::Realizer::Adapter::ActiveRecord
+  def paginate(scope, per, offset)
+    scope.page(offset.to_i + 1).per(per)
+  end
+end
