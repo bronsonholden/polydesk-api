f9bff4ae21ea405584bc16a58937415d3772c556
Paul Holden <paul@holdensoftware.com>
2019-08-03 22:12:03 -0700

    Clean up Folder model tests

--- spec/models/folders_spec.rb
+++ spec/models/folders_spec.rb
@@ -1,32 +1,37 @@
 require 'rails_helper'
 
 describe Folder do
-  describe 'delete with subfolder' do
-    let!(:folder) { create :subfolder }
-    it 'deletes both folders' do
-      expect { folder.folder.destroy }.to change(Folder, :count).by(-2)
+  let(:folder) { create :folder }
+  let(:subfolder) { create :subfolder }
+  let(:subdocument) { create :subdocument }
+
+  describe 'deletion' do
+    context 'with subfolder' do
+      it 'deletes both folders' do
+        subfolder
+        expect { subfolder.folder.destroy }.to change(Folder, :count).by(-2)
+      end
+    end
+
+    context 'with parent' do
+      it 'retains parent folder' do
+        subfolder
+        expect { subfolder.destroy }.to change(Folder, :count).by(-1)
+      end
+    end
+
+    context 'with document' do
+      it 'deletes folder and document' do
+        subdocument
+        expect { subdocument.folder.destroy }.to change(Document, :count).by(-1)
+      end
     end
   end
 
-  describe 'delete with parent' do
-    let!(:folder) { create :subfolder }
-    it 'retains parent folder' do
-      expect { folder.destroy }.to change(Folder, :count).by(-1)
-    end
-  end
-
-  describe 'delete with document' do
-    let!(:document) { create :subdocument }
-    it 'deletes folder and document' do
-      expect { document.folder.destroy }.to change(Document, :count).by(-1)
-    end
-  end
-
-  describe 'validations' do
+  describe 'validation' do
     context 'with parent folder' do
-      let!(:folder) { create :subfolder, name: 'Subfolder' }
       it 'prevents duplicate name' do
-        expect { folder.folder.folders.create!(name: 'Subfolder') }.to raise_error(ActiveRecord::RecordInvalid)
+        expect { subfolder.folder.folders.create!(name: subfolder.name) }.to raise_error(ActiveRecord::RecordInvalid)
       end
 
       it 'disallows being own parent' do
@@ -35,23 +40,23 @@ describe Folder do
     end
 
     context 'without parent folder' do
-      let!(:folder) { create :folder, name: 'Folder' }
       it 'prevents duplicate name' do
-        expect { Folder.create!(name: 'Folder') }.to raise_error(ActiveRecord::RecordInvalid)
+        expect { Folder.create!(name: folder.name) }.to raise_error(ActiveRecord::RecordInvalid)
       end
     end
 
     context 'when discarded' do
-      let!(:discarded) { create :discarded_folder, name: 'Discarded Folder' }
-      let!(:undiscarded) { create :folder, name: 'Undiscarded Folder' }
+      let(:discarded) { create :discarded_folder, name: 'Discarded Folder' }
+      let(:undiscarded) { create :folder, name: 'Undiscarded Folder' }
       # To undiscard later, to verify duplicate names not allowed on restore
-      let!(:to_undiscard) { create :discarded_folder, name: 'Undiscarded Folder' }
+      let(:to_undiscard) { create :discarded_folder, name: 'Undiscarded Folder' }
 
       it 'allows duplicate name' do
         expect { Folder.create!(name: 'Discarded Folder').discard! }.not_to raise_error
       end
 
       it 'disallows restoring with duplicate name' do
+        undiscarded
         expect { to_undiscard.undiscard! }.to raise_error(ActiveRecord::RecordInvalid)
       end
 
