3b2870a33e0c2a6533dca49a12f7c2e89344a447
Paul Holden <paul@holdensoftware.com>
2019-12-29 23:00:08 -0800

    Not dealing with CI for now
    Not sure why Hash comparison is misbehaving when the tests run in CI, but it
    would be better to have the tests compare the actual values of attributes
    instead of the whole hash. Will deal with later.

--- spec/support/json_api_matchers.rb
+++ spec/support/json_api_matchers.rb
@@ -42,8 +42,6 @@ RSpec::Matchers.define :have_changed_attributes do
     Apartment::Tenant.switch('rspec') do
       before = record.attributes.except('updated_at')
       after = record.reload.attributes.except('updated_at')
-      puts before
-      puts after
       return before != after
     end
   end
