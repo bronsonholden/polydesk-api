60bc2d049c1c9729d85f717400df61600169c4d7
Paul Holden <paul@holdensoftware.com>
2019-12-29 22:39:07 -0800

    Tweak rspec matcher have_changed_attributes
    Was failing in CI since updated_at was being compared, and the update request
    was "slow" enough in the CI VM for it to be different than its initial value.

--- spec/support/json_api_matchers.rb
+++ spec/support/json_api_matchers.rb
@@ -40,7 +40,9 @@ end
 RSpec::Matchers.define :have_changed_attributes do
   match do |record|
     Apartment::Tenant.switch('rspec') do
-      return record.attributes != record.reload.attributes
+      before = record.attributes.except('updated_at')
+      after = record.reload.attributes.except('updated_at')
+      return before != after
     end
   end
 end
