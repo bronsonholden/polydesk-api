b28f152a74c33d1ded6d95be31f21801abdcecdb
Paul Holden <paul@holdensoftware.com>
2020-01-28 17:56:18 -0800

    Add timestamps to Blueprint & Prefab serializers

--- app/serializers/blueprint_serializer.rb
+++ app/serializers/blueprint_serializer.rb
@@ -1,3 +1,3 @@
 class BlueprintSerializer < TenantSerializer
-  attributes :name, :namespace, :schema, :view, :construction_view
+  attributes :name, :namespace, :schema, :view, :construction_view, :created_at, :updated_at
 end
--- app/serializers/prefab_serializer.rb
+++ app/serializers/prefab_serializer.rb
@@ -1,4 +1,4 @@
 class PrefabSerializer < TenantSerializer
-  attributes :namespace, :tag, :schema, :view, :data
+  attributes :namespace, :tag, :schema, :view, :data, :created_at, :updated_at
   has_one :blueprint, class_name: 'Blueprint'
 end
