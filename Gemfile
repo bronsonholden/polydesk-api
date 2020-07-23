fed73e986921914a7c73325b4b2ff1dab91a92ba
Paul Holden <paul@holdensoftware.com>
2020-07-23 13:31:04 -0700

    Seeding

--- Gemfile
+++ Gemfile
@@ -65,6 +65,7 @@ gem 'smush'
 gem 'auto_increment'
 gem 'keisan'
 gem 'pg_party'
+gem 'activerecord-import'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,72 @@
 
 require 'securerandom'
 
-blueprint = Blueprint.all.first
-if blueprint.nil?
-  blueprint = Blueprint.create(name: 'Blueprint', namespace: 'prefabs', schema: { type: 'object' }, view: { xs: {} })
+num_employees = 50000
+num_jobs = 5000
+
+employee_blueprint = Blueprint.create!(name: 'Employee', namespace: 'employees', schema: { type: 'object' }, view: { xs: {} })
+job_blueprint = Blueprint.create!(name: 'Job', namespace: 'jobs', schema: { type: 'object' }, view: { xs: {} })
+
+jobs = []
+
+num_jobs.times do |i|
+  puts "JOB #{i}"
+  jobs << [
+    i,
+    [],
+    job_blueprint.id,
+    'jobs',
+    {},
+    {},
+    {
+      job_name: "JOB #{SecureRandom.hex}"
+    }
+  ]
 end
-n = 500000
-# 10000000
-first = Prefab.all.size + 1
-last = first + n
-refs = (first...last).to_a.shuffle
-n.times do |i|
-  puts "Prefab ##{i + first}"
-  Prefab.create!(blueprint: blueprint, data: {
-    prefab: "prefabs/#{refs[i]}",
-    value: SecureRandom.hex
-  })
+
+employees = []
+num_employees.times do |i|
+  puts "EMPLOYEE #{i}"
+  employees << [
+    i,
+    [],
+    employee_blueprint.id,
+    'employees',
+    {},
+    {},
+    {
+      first_name: "FIRST #{SecureRandom.hex}",
+      last_name: "LAST #{SecureRandom.hex}",
+      job: "jobs/#{rand(1..num_jobs)}"
+    }
+  ]
 end
+
+Prefab.import [ :id, :flat_data, :blueprint_id, :namespace, :schema, :view, :data ], jobs
+Prefab.import [ :id, :flat_data, :blueprint_id, :namespace, :schema, :view, :data ], employees
+
+# PrefabQuery.new({ 'generate' => { 'job_name' => 'lookup_s("data.job", "data.job_name")' } }, inner_scope: Prefab.all).apply(Prefab.partition_key_eq('employees'))
+# PrefabQuery.new({ 'generate' => { 'job_name' => 'lookup_s("data.job", "data.job_name")' } }, inner_scope: Prefab.all).apply(Prefab.partition_key_eq('employees')).map { |e| "#{e.data['first_name']} - #{e.job_name}" }
+# PrefabQuery.new({ 'generate' => { 'employee_count' => 'referent_count_distinct("employees", "data.job", "id")' } }, inner_scope: Prefab.all).apply(Prefab.partition_key_eq('jobs'))
+# PrefabQuery.new({ 'generate' => { 'employee_count' => 'referent_count_distinct("employees", "data.job", "id")' } }, inner_scope: Prefab.all).apply(Prefab.partition_key_eq('jobs')).to_a.map { |j| "#{j.data['job_name']} - #{j.employee_count}" }
+
+#
+# blueprint = Blueprint.all.first
+# if blueprint.nil?
+#   blueprint = Blueprint.create(name: 'Blueprint', namespace: 'prefabs', schema: { type: 'object' }, view: { xs: {} })
+# end
+# n = 500000
+# # 10000000
+# first = Prefab.all.size + 1
+# last = first + n
+# refs = (first...last).to_a.shuffle
+# n.times do |i|
+#   puts "Prefab ##{i + first}"
+#   Prefab.new(blueprint: blueprint, data: {
+#     prefab: "prefabs/#{refs[i]}",
+#     value: SecureRandom.hex
+#   })
+# end
 
 # expr = 'lookup_s("data.prefab", "data.value")'
 # generate = { 'generate' => { 'lookup_value' => expr } }
